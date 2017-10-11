e3d53f5bf4716f74a398a580d354ea9ef6604d39
fengahan <544976880@qq.com>
2017-10-11 15:36:00 +0800

    new_file

---
+++ str_repeat.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"bytes"
+)
+
+/**
+https://secure.php.net/manual/zh/function.str-repeat.php
+str_repeat — 重复一个字符串
+ */
+func main() {
+
+	str:="abc";
+	new_str:=str_repeat(str,6)
+	fmt.Println(new_str)
+}
+
+func str_repeat (str string,mult int)string{
+	new_str:=""
+	if mult<1 {
+		return new_str
+	}
+	var buffer bytes.Buffer
+	for i:=0;i<mult;i++  {
+		buffer.WriteString(str)
+	}
+	new_str=buffer.String()
+	return new_str
+}
