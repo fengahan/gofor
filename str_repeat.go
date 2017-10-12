5d51e397f97e8546dfcfd5c4204247d3936225c5
fengahan <544976880@qq.com>
2017-10-12 23:48:26 +0800

    增加注释（go语言自带函数可替换某些自定义函数）

--- str_repeat.go
+++ str_repeat.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"bytes"
+	"strings"
 )
 
 /**
@@ -13,11 +14,12 @@ str_repeat — 重复一个字符串
 func main() {
 
 	str:="abc";
-	new_str:=str_repeat(str,6)
+	new_str:=str_repeat(str,6)//	strings.Repeat()可用此函数代替
 	fmt.Println(new_str)
 }
 
 func str_repeat (str string,mult int)string{
+
 	new_str:=""
 	if mult<1 {
 		return new_str
