8a86c77964edbc41b3ed5e07a0d404c75b72e842
fengahan <544976880@qq.com>
2017-10-12 23:50:22 +0800

    修该备注

--- str_repeat.go
+++ str_repeat.go
@@ -2,24 +2,18 @@ package main
 
 import (
 	"fmt"
-
 	"bytes"
-	"strings"
 )
-
 /**
 https://secure.php.net/manual/zh/function.str-repeat.php
 str_repeat — 重复一个字符串
  */
 func main() {
-
-	str:="abc";
+	str:="abc"
 	new_str:=str_repeat(str,6)//	strings.Repeat()可用此函数代替
 	fmt.Println(new_str)
 }
-
 func str_repeat (str string,mult int)string{
-
 	new_str:=""
 	if mult<1 {
 		return new_str
